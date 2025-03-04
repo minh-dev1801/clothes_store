6d80a0dabc95567d91c697249ad8e4ec6df9bf4d
minh-dev1801 <minhnv1801@gmail.com>
2025-03-04 11:30:01 +0700

    Configure Prisma with Neon serverlesss and fetch products data via server actions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,10 @@
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+  console.log(latestProducts);
+
   return <ProductList />;
 };
 
---
+++ db/prisma.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+
+const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
+
+export default prisma;
